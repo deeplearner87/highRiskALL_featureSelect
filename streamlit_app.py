3b5daffb8ead4d62409a110508e8d1fa04fd7d87
Dibyendu B. Seal, Ph.D. <dibs.adi@gmail.com>
2025-02-19 15:06:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,12 @@ def mapping_Proteomics_DRP_to_metadata(drugOfInterest):
         with open("metadata.xlsx", "wb") as f:
             f.write(response.content)
     #Read the file
-    metadata = pd.read_excel("metadata.xlsx", header=0)
+    #metadata = pd.read_excel("metadata.xlsx", header=0)
+    try:
+        metadata = pd.read_excel("metadata.xlsx", engine="openpyxl")
+        st.print(metadata.head())  # Print first few rows to confirm it's loaded
+    except Exception as e:
+        st.print(f"Error reading Excel file: {e}")
     metadata['Sample ID Proteomics'] = metadata['Sample ID Proteomics'].astype('str')
     metadata['Sample ID Proteomics'] = 'S'+metadata['Sample ID Proteomics']
     metadata['Immunophenoytpe']=metadata['Immunophenoytpe'].astype('str')
